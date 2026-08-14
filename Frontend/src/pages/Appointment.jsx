1b07aa7bc4dcb14f3b52d376e9c65617f012ed98
Aryan Kumar <aryankr0703@gmail.com>
2026-08-15 03:21:25 +0530

    Frontend progress till appointment page

--- Frontend/src/pages/Appointment.jsx
+++ Frontend/src/pages/Appointment.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { AppContext } from '../context/Appcontext'
 
 const Appointment = () => {
-  return (
+
+  const {docId} = useParams()
+  const {doctors} = useContext(AppContext)
+  const [docInfo,setDocInfo] = useState(null)
+
+  const fetchDocInfo = async () => {
+    const docInfo = doctors.find(doc => doc._id === docId)
+    setDocInfo(docInfo)
+    console.log(docInfo)
+  }
+
+  useEffect(()=>{
+    fetchDocInfo()
+  },[doctors,docId])
+
+  return docInfo &&(
     <div>
-        
+        {/*--------------Doctor-Details------------------*/}
+        <div>
+          <div>
+            <img src={docInfo.image} alt="" />
+          </div>
+
+          <div>
+            {/*--------------------Doc Info:Name,degree, Exprience----------------*/}
+          </div>
+        </div>
     </div>
   )
 }
